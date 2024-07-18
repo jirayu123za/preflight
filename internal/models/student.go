72db27d1311e015bb11452663a3f33054abf70b5
jirayu <jirayu004za@gmail.com>
2024-07-19 00:07:02 +0700

    change StudentID from unit to string

--- internal/models/student.go
+++ internal/models/student.go
@@ -4,7 +4,7 @@ import "gorm.io/gorm"
 
 type Student struct {
 	gorm.Model
-	StudentID uint   `json:"student_id" gorm:"primaryKey"`
+	StudentID string `json:"student_id" gorm:"primaryKey"`
 	FirstName string `json:"first_name"`
 	LastName  string `json:"last_name"`
 	Email     string `json:"email" gorm:"unique"`
